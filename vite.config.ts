3dd6b3d99988a78b4f59c9d18214b042ece76bce
RobinFungai <robin@fungai.art>
2026-04-25 22:55:52 +0200

    REAL FIX: Changed import to plugin-react-swc and added cache buster

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// Forced update to clear GitHub cache
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc' // <--- DOUBLE CHECK THIS WORD
 import path from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './', 
