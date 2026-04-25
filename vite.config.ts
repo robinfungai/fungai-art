8e2d6eb9c438fb1664f1511c22d2f6e30d074ca3
RobinFungai <robin@fungai.art>
2026-04-26 00:18:37 +0200

    FIX: Correcting plugin name on master

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'  // Changed from @vitejs/react-swc
+import react from '@vitejs/plugin-react-swc'  // Add "plugin" here
 import path from "path"
 
 export default defineConfig({
